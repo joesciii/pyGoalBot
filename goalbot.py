d8a9ef44b0b60dd5dd8346fcd54887e1a250d769
joe scibetta <joedscibetta@hotmail.com>
2021-09-26 18:44:40 +0100

    changed to asyncpraw and added discordpy

--- goalbot.py
+++ goalbot.py
@@ -1,11 +1,13 @@
+from asyncio.tasks import sleep
 import discord
-from discord import team
-import praw
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import asyncpraw
 import requests
 import json
 
-#array of teams
-#testing below, grab json, filter to teams, store in array
+
 response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
 
 teams = []
@@ -18,7 +20,7 @@ if response.status_code == requests.codes.ok:
 mediaSites = ['streamwo.com', 'streamja.com', 'streamable.com', 'stream', 'clippituser.tv']
 
 
-reddit = praw.Reddit(
+reddit = asyncpraw.Reddit(
     client_id="your_redditbot_id",
     client_secret="your_redditbot_secret",
     user_agent="Goal Bot",
@@ -26,27 +28,55 @@ reddit = praw.Reddit(
 
 submissionURL = ""
 
-subreddit = reddit.subreddit("soccer")
-for submission in subreddit.stream.submissions():
+async def search_subreddit():
+    subreddit = await reddit.subreddit("soccer")
+    async for submission in subreddit.stream.submissions():
         for t in teams:
             if t in submission.title:
                 for s in mediaSites:
                     if s in submission.url:
                         submissionURL = submission.url
-
-print(submissionURL)        
-
-
-            
-
-""" client = discord.client()
+                        print("URL A: " + submissionURL)
+                        return submissionURL
+                            
+                            
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event """
+async def run_in_background():
+    oldURL = ""
+    await client.wait_until_ready()
+    channel = client.get_channel(id=728287924495319120) # replace with channel_id
+    submissionURL = await search_subreddit()
+    while not client.is_closed():
+        
+        if oldURL != submissionURL:
+            await channel.send(submissionURL)   
+        await asyncio.sleep(5) # task runs every 30 seconds
+        oldURL = submissionURL
+        print("Old URL: " + oldURL)
+        print("URL: " + submissionURL)
 
-#send message of 
+
+
+client.loop.create_task(run_in_background())
+client.run('your_discord_token')
+
+
+    
+
+
+
+
+
+
+
+
+
+            
+
 
  

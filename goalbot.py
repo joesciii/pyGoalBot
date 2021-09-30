3cabad978ef9265670bf5d2ec8cc3d184f38e292
joe scibetta <joedscibetta@hotmail.com>
2021-09-30 18:37:50 +0100

    added check embed creation to ensure message sending

--- goalbot.py
+++ goalbot.py
@@ -13,7 +13,7 @@ if response.status_code == requests.codes.ok:
     teams = [t['name'] for t in jsonResponse['teams']]
     fullManc = "Manchester"
     fullSpurs = "Tottenham"
-    teams += [fullManc, fullSpurs]
+    teams += [fullManc, fullSpurs] #response only returns 'Man City, Man U, Spurs'
 
 else:
     print("Cannot access Premier League API to grab active teams. Try again later or manually populate the 'teams' list.")
@@ -26,6 +26,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("pyGoalBot connected")
+    #add reddit bot info below
     reddit = asyncpraw.Reddit(
         client_id="your_redditbot_id",
         client_secret="your_redditbot_secret",
@@ -36,21 +37,27 @@ async def on_ready():
     
 
     subreddit = await reddit.subreddit("soccer")
-    channel = client.get_channel(id=728287924495319120) # replace with channel_id
+    channel = client.get_channel(id=728287924495319120) # replace with your channel_id
     
     async for submission in subreddit.stream.submissions():
         for t in teams:
-            if t in submission.title:
-                if " W " not in submission.title:
-                    for s in mediaSites:
-                        if s in submission.url:
-                            newSubmission = submission.title + " - " + submission.url
+            if t in submission.title and " W " not in submission.title:
+                for s in mediaSites:
+                    if s in submission.url:
+                        newSubmission = submission.title + " - " + submission.url
+                            
         if oldSubmission != newSubmission:
-            embed=discord.Embed(title="GOAL")
-            embed.add_field(name=submission.title, value=submission.url, inline=False)
-            await channel.send(embed=embed)
-            print("New submission: " + newSubmission)
-            await asyncio.sleep(1)
-            oldSubmission = newSubmission 
+            if len(submission.title) <= 128:
+                embed=discord.Embed(title="GOAL")
+                embed.add_field(name=submission.title, value=submission.url, inline=False)
+                await channel.send(embed=embed)
+                print("New submission: " + newSubmission)
+                oldSubmission = newSubmission 
+                await asyncio.sleep(1)               
+            else:
+                await channel.send(newSubmission)
+                print("New submission: " + newSubmission)
+                oldSubmission = newSubmission 
+                await asyncio.sleep(1)      
 
-client.run('your_discord_token')
+client.run('your_discordbot_token')
